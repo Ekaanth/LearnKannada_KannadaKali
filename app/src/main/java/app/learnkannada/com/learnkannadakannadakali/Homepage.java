0281e14be804caa7a861a8f659f009f952f72bdd
vaam <varun.a_m@nokia.com>
2017-11-03 17:16:33 +0530

    added everything to string.xml

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/Homepage.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/Homepage.java
@@ -22,6 +22,7 @@ public class Homepage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Homepage.this, MainActivity.class));
+
             }
         });
 
