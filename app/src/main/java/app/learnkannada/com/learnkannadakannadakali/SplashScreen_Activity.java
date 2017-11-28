09fa552d8b79e12cc1c3d30352655ab63b3e85e5
raggitha <Aggithaya123>
2017-11-28 22:30:11 +0530

    changes to most activities

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/SplashScreen_Activity.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/SplashScreen_Activity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class SplashScreen_Activity extends AppCompatActivity {
 
     private static final int WELCOME_DURATION = 2000;
     private GIFView flagGif;
+    private ImageView hitham;
+    private Animation fromBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class SplashScreen_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen_);
 
         flagGif = (GIFView) findViewById(R.id.welcomeFlagID);
+        hitham = (ImageView) findViewById(R.id.logoID);
+
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.from_bottom);
+        hitham.setAnimation(fromBottom);
 
         //getSupportActionBar().hide();
 
