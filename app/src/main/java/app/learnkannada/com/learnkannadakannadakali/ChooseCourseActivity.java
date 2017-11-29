cf95589a9f1274138dd2fc756d7e9296e2ed945b
vaam <varunvgnc@gmail.com>
2017-11-29 18:11:24 +0530

    latest as of now

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/ChooseCourseActivity.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/ChooseCourseActivity.java
@@ -204,13 +204,29 @@ public class ChooseCourseActivity extends AppCompatActivity {
             startActivity(intent);
             return true;
         }
-        else if (item.getItemId() == R.id.settingsID)
+       /* else if (item.getItemId() == R.id.settingsID)
         {
             infoBuilder.setTitle("Settings");
             LayoutInflater inflater = this.getLayoutInflater();
             View dialogView = inflater.inflate(R.layout.settings_dialog, null);
             infoBuilder.setView(dialogView);
             infoBuilder.create().show();
+        }*/
+        else if (item.getItemId() == R.id.rateID)
+        {
+            Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
+            }
         }
         return super.onOptionsItemSelected(item);
     }
