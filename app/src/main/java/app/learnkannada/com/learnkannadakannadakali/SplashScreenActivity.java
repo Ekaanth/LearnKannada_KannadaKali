9bf00b9fbaa7285200d780bf5edeeaf3414e48f9
raggitha <Aggithaya123>
2017-12-18 16:14:42 +0530

    alphabets and sounds

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/SplashScreenActivity.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package app.learnkannada.com.learnkannadakannadakali;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +14,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.flaviofaria.kenburnsview.KenBurnsView;
-import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int WELCOME_DURATION = 5000;
+    private static final int WELCOME_DURATION = 7000;
     private ImageView hitham;
     private TextView splashText;
     private KenBurnsView kenBurnsView;
@@ -28,24 +29,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen_);
 
         //flagGif = (GIFView) findViewById(R.id.welcomeFlagID);
-        //compile 'com.flaviofaria:kenburnsview:1.0.6'
-        //add above dependency to the build
+
         splashText = (TextView)findViewById(R.id.welcomeID);
         hitham = (ImageView) findViewById(R.id.logoID);
-        kenBurnsView = (KenBurnsView)findViewById(R.id.kbimageID);
+
 
         //This code is to increase initiate and increase the duration of the effect.
-        AccelerateDecelerateInterpolator acc_dec = new AccelerateDecelerateInterpolator();
-        RandomTransitionGenerator generator = new RandomTransitionGenerator(5000,acc_dec);
-        kenBurnsView.setTransitionGenerator(generator);
+        //setAnimation();
+        kenBurnsView = (KenBurnsView)findViewById(R.id.kbimageID);
+        kenBurnsView.setImageResource(R.drawable.splashcollage);
         //end of kbview code
 
         Animation myTransition = AnimationUtils.loadAnimation(this,R.anim.my_transition);
         hitham.setAnimation(myTransition);
         splashText.setAnimation(myTransition);
-
-
-        //getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -55,4 +52,20 @@ public class SplashScreenActivity extends AppCompatActivity {
             }
         }, WELCOME_DURATION);
     }
+
+//   private void setAnimation() {
+//        ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcomeID), "scaleX", 5.0F, 1.0F);
+//        scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+//        scaleXAnimation.setDuration(1200);
+//        ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcomeID), "scaleY", 5.0F, 1.0F);
+//        scaleYAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+//        scaleYAnimation.setDuration(1200);
+//        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcomeID), "alpha", 0.0F, 1.0F);
+//        alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+//        alphaAnimation.setDuration(1200);
+//        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
+//        animatorSet.setStartDelay(500);
+//        animatorSet.start();
+//    }
 }
