b648650009e4217fc316944668a9a640bcdd98b8
vaam <varunvgnc@gmail.com>
2018-02-23 20:58:08 +0530

    added search option to antonyms

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/AntonymsActivity.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/AntonymsActivity.java
@@ -7,9 +7,12 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
@@ -26,6 +29,24 @@ public class AntonymsActivity extends AppCompatActivity {
 
     private List<String> word = new ArrayList<>();
     private List<String> antonym = new ArrayList<>();
+    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String wordEng = intent.getStringExtra(Constants.WORDS);
+            String antonymEng = intent.getStringExtra(Constants.ANTONYMS);
+            String wordKan, antonymKan;
+
+            String[] wordKanArray = getResources().getStringArray(R.array.pWords_array_kan);
+            String[] antonymKanArray = getResources().getStringArray(R.array.nWords_array_kan);
+            wordKan = wordKanArray[word.indexOf(wordEng)];
+            antonymKan = antonymKanArray[antonym.indexOf(antonymEng)];
+
+            AntonymTopFragment fr = (AntonymTopFragment) getFragmentManager().findFragmentById(R.id.fr_id);
+            fr.updateValues(wordEng, antonymEng, wordKan, antonymKan);
+        }
+    };
+    private ListViewAdapter adapter;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,43 +69,50 @@ public class AntonymsActivity extends AppCompatActivity {
             antonym.add(antonymsList[i]);
         }
 
-        RecyclerView.Adapter adapter = new ListViewAdapter(getApplicationContext(), word, Constants.ANTONYMS, antonym);
+        adapter = new ListViewAdapter(getApplicationContext(), word, Constants.ANTONYMS, antonym);
         recyclerView.setAdapter(adapter);
 
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter(Constants.FROM_ADAPTER));
 
     }
 
-    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String wordEng = intent.getStringExtra(Constants.WORDS);
-            String antonymEng = intent.getStringExtra(Constants.ANTONYMS);
-            String wordKan ,antonymKan;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recycler, menu);
+        MenuItem item = menu.findItem(R.id.searchID);
 
-            String[] wordKanArray = getResources().getStringArray(R.array.pWords_array_kan);
-            String[] antonymKanArray = getResources().getStringArray(R.array.nWords_array_kan);
-            wordKan = wordKanArray[word.indexOf(wordEng)];
-            antonymKan = antonymKanArray[antonym.indexOf(antonymEng)];
-
-            AntonymTopFragment fr = (AntonymTopFragment) getFragmentManager().findFragmentById(R.id.fr_id);
-            fr.updateValues(wordEng,antonymEng,wordKan,antonymKan);
-        }
-    };
+        searchView = (SearchView) MenuItemCompat.getActionView(item);
+        doSearch(searchView);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
-            return  true;
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void doSearch(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+    }
+
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(AntonymsActivity.this,FlexiCourseHomeActivity.class));
+        startActivity(new Intent(AntonymsActivity.this, FlexiCourseHomeActivity.class));
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
         finish();
     }
