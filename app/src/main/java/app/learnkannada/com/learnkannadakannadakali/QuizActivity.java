9adc5fdef5198185f48c89e7560c7a687a81cf5f
raggitha <Aggithaya123>
2018-04-05 19:09:37 +0530

    fixes for issues mentioned in keep and also addition of few strings

--- app/src/main/java/app/learnkannada/com/learnkannadakannadakali/QuizActivity.java
+++ app/src/main/java/app/learnkannada/com/learnkannadakannadakali/QuizActivity.java
@@ -5,6 +5,7 @@ import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -12,8 +13,10 @@ import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -22,6 +25,7 @@ import android.support.v7.widget.CardView;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -69,6 +73,7 @@ public class QuizActivity extends AppCompatActivity implements LoaderManager.Loa
     private AlertDialog.Builder builder;
     private SharedPreferences levelPrefs;
 
+    private static final int WRITE_STORAGE_PERMISSION_GRANTED = 100, READ_STORAGE_PERMISSION_GRANTED = 101;
     final Bundle loaderBundle = new Bundle();
     final LoaderManager loaderManager = getSupportLoaderManager();
     private boolean chosenAnswer = false, dataLoaded = false;
@@ -345,7 +350,22 @@ public class QuizActivity extends AppCompatActivity implements LoaderManager.Loa
         shareQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                shareResult((questionsAnsweredCorrect * 100) / listOfQuestions.size());
+                    if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                            PackageManager.PERMISSION_GRANTED &&
+                            ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                                    PackageManager.PERMISSION_GRANTED)
+                    {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(QuizActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                        } else
+                            ActivityCompat.requestPermissions(QuizActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_STORAGE_PERMISSION_GRANTED);
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(QuizActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                        } else
+                            ActivityCompat.requestPermissions(QuizActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE_PERMISSION_GRANTED);
+                    } else
+                        shareResult((questionsAnsweredCorrect * 100) / listOfQuestions.size());
             }
         });
 
@@ -638,6 +658,43 @@ public class QuizActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_quiz_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.helpID) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                } else
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_STORAGE_PERMISSION_GRANTED);
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                } else
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE_PERMISSION_GRANTED);
+            } else
+                shareScreenShot();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareScreenShot() {
+        Toast.makeText(this, "shareScreenShot()", Toast.LENGTH_SHORT).show();
+        View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        Bitmap screenShotBitMap = getScreenShot(rootView);
+        String currentTimeStamp = (String) DateFormat.format("MMddyyhhmmss", new Date().getTime());
+        storeAndShare(screenShotBitMap, "LearnKannadaSmartapp_" + currentTimeStamp + ".jpg", getString(R.string.helpShareBody));
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         int score = data.getIntExtra("score", 0);
@@ -685,15 +742,6 @@ public class QuizActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==android.R.id.home)
-        {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onBackPressed() {
